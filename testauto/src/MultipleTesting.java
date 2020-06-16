bdeb4acd480c2bf2306e5d2ef9eea61855a96c6a
AshokMaddineni <ashokkumarmaddineni@gmail.com>
2020-06-16 20:49:05 +0530

    Added author

--- testauto/src/MultipleTesting.java
+++ testauto/src/MultipleTesting.java
@@ -10,7 +10,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+/*
+ * @author = AshokMaddineni 
+ * @Description= This Test case with the halep of excel file can load the data
+ * */
 public class MultipleTesting {
 
 	
