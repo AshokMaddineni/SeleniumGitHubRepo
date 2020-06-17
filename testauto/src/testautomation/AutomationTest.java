d29e48fe14946d0ac71aad3156fa033156f656e3
AshokMaddineni <ashokkumarmaddineni@gmail.com>
2020-06-17 12:08:18 +0530

    Commited and TestNGfile

--- testauto/src/testautomation/AutomationTest.java
+++ testauto/src/testautomation/AutomationTest.java
@@ -4,10 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class AutomationTest {
 
-	public static void main(String[] args) {
+	@Test
+	public void Gmail() {
 		// TODO Auto-generated method stub
 
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\maddi\\Desktop\\Selenium\\chromedriver.exe");
